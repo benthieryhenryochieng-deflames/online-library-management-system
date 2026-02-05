837f51867355e57baf52ca42719af858f23d34ae
benthieryhenryochieng-deflames <benthieryhenryochieng@gmail.com>
2026-02-05 22:02:10 +0300

    Add files via upload

---
+++ user.cpp
@@ -0,0 +1,19 @@
+#ifndef USER_H
+#define USER_H
+
+
+#include<string>
+using namespace std;
+class user
+{
+	private:
+		int userId, string name;
+	public:
+		user(int id,string name);
+		int getId()const;
+		string getname()const
+		
+};
+
+#endif
+
